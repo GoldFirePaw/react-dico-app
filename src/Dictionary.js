bddcb76cd82feab3519a30c843e9b242dc788e10
GoldFirePaw <camille.flajollet@gmail.com>
2022-04-29 15:29:59 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -15,23 +15,33 @@ export default function Dictionary() {
         if (word) {
             let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
             axios.get(apiUrl).then(function (response) {
-                setData(response.data[0]);
+                setData(response);
                 console.log(response)
+            }).catch(function (error) {
+                setError(error.response.data)
             })
         }
 
     }, [word])
 
+    function trueOrFalse() {
+        if (data) {
 
+            return (<>
+                <Prononciation error={false} data={true} word={word} audio={data.data[0].phonetics} />
+                <Meanings error={false} data={true} meanings={data.data[0].meanings} />
+                <Pictures error={false} data={true} />
+            </>)
+        }
+
+        if (error) {
+            return (<Error error={true} data={false} title={error.title} message={error.message} resolution={error.resolution} />
+            )
+        }
+    }
 
     return (<div className='app-container'>
         <Search setWord={setWord} word={word} />
-        {data &&
-            <>
-                <Prononciation word={word} audio={data.phonetics} />
-                <Meanings meanings={data.meanings} />
-                <Pictures />
-            </>
-        }
+        <div>{trueOrFalse()}</div>
     </div>)
 }
